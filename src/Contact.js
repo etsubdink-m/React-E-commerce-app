6e49b99b595e0f13ab0ecc6fee85155bf5f683b5
Etsubdink Mersha <93763013+etsubdink-m@users.noreply.github.com>
2023-11-06 11:37:29 -0900

    Update Contact.js

--- src/Contact.js
+++ src/Contact.js
@@ -36,7 +36,9 @@ const Contact = () => {
   return (
     <Wrapper>
       <h2 className="common-heading">Contact page</h2>
-    
+    <div>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31524.439800826956!2d38.73328334511033!3d9.013031673778933!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x164b85b915db8c8b%3A0xd8577380eddaf012!2sMeskel%20Square!5e0!3m2!1sen!2set!4v1699303024804!5m2!1sen!2set" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    </div>
       <div className="container">
         <div className="contact-form">
           <form
