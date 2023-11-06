f37f62df098baa08e30272433722f2dd4ae504ff
Etsubdink Mersha <93763013+etsubdink-m@users.noreply.github.com>
2023-11-06 11:29:46 -0900

    Update Contact.js

--- src/Contact.js
+++ src/Contact.js
@@ -36,8 +36,7 @@ const Contact = () => {
   return (
     <Wrapper>
       <h2 className="common-heading">Contact page</h2>
-<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d252230.0201920376!2d38.61367828187112!3d8.96347968311767!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x164b85cef5ab402d%3A0x8467b6b037a24d49!2sAddis%20Ababa!5e0!3m2!1sen!2set!4v1699302361395!5m2!1sen!2set" width="100%" height="400" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d252230.0201920376!2d38.61367828187112!3d8.96347968311767!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x164b85cef5ab402d%3A0x8467b6b037a24d49!2sAddis%20Ababa!5e0!3m2!1sen!2set!4v1699302563567!5m2!1sen!2set" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
       <div className="container">
         <div className="contact-form">
           <form
